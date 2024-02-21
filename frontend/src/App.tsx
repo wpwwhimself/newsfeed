213e4b943c0fb7951f585e373292c8c95cc940d3
Wojciech Przybyła <contact@wpww.pl>
2024-02-21 18:45:41 +0100

    💄 basics of layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import './App.css';
 import { TopHeader } from './components/TopHeader';
 import { ArticleList } from './components/ArticleList';
+import { Pill } from './components/Pill';
 
 function App() {
   return (
     <div className="App flex-down">
       <TopHeader label="NewsFeed" />
-      <ArticleList />
+      <div className="main-wrapper">
+        <div className="sidebar flex-down">
+          <Pill>aaa</Pill>
+        </div>
+        <div className="main-content">
+          <ArticleList />
+        </div>
+      </div>
     </div>
   );
 }
