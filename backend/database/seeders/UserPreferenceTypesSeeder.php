d2137d8c862961651322c75d8300842668b77afd
Wojciech Przybyła <contact@wpww.pl>
2024-02-22 20:44:37 +0100

    🚸 user preferences - database considerations

---
+++ backend/database/seeders/UserPreferenceTypesSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserPreferenceTypesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table("user_preference_types")->insert([
+            ["name" => "source"],
+            ["name" => "category"],
+            ["name" => "author"],
+        ]);
+    }
+}
