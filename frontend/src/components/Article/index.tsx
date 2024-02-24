2527b63fe2bd69b1e21f208cf8c131e1aa1aecc1
Wojciech Przybyła <contact@wpww.pl>
2024-02-24 20:43:28 +0100

    💄 article - buttons and alignment

--- frontend/src/components/Article/index.tsx
+++ frontend/src/components/Article/index.tsx
@@ -1,7 +1,11 @@
+import "./style.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { date } from "../../helpers/dateFormat"
 import { ArticleProps } from "../../types"
+import { Button } from "../Button"
 import { Notification } from "../Notification"
 import { Pill } from "../Pill"
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons"
 
 interface Props {
   article?: ArticleProps,
@@ -15,16 +19,24 @@ export const Article = ({article}: Props) => {
 
   const {title, description, content, source, author, url, url_to_image, published_at} = article
 
+  const openLink = (link: string) => {
+    window.open(link, "_blank")
+  }
+
   return <Pill>
-    <div className="flex-down">
+    <div className="article flex-down">
       <img src={url_to_image} alt={title} />
-      <span className="title level-2">{title}</span>
+      <span className="title level-2 accent">{title}</span>
       <span className="title level-3">{description}</span>
       <span className="ghost">by {author} • {date(published_at)}</span>
 
       <p>{content}</p>
 
-      <span className="ghost">Source: <a href={url}>{source}</a></span>
+      <Button
+        icon={<FontAwesomeIcon icon={faArrowUpRightFromSquare} />}
+        label={`Read more: ${source}`}
+        onClick={() => openLink(url)}
+      />
     </div>
   </Pill>
 }
